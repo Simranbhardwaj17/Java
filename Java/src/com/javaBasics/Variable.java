13170181294ae55f9395b45c20c408bc700ca5fe
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-07-29 16:46:05 +0530

    Variable

---
+++ Java/src/com/javaBasics/Variable.java
@@ -0,0 +1,21 @@
+package com.javaBasics;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		//variable
+		String name = "Anshu";
+		int age = 30;
+		String niece = "Simi";
+		String friend = niece;  //assign one var value to other (data type should same)
+		
+        //String friend = age;      differ DT
+		
+        System.out.println(friend); //ln for line break
+		
+		System.out.print("Hello, Simran!"); 
+
+	}
+
+}
