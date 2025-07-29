b5df35c416360401b93b185587435e9dab60ccdb
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-07-29 16:34:07 +0530

    First java program

---
+++ Java/src/com/javaBasics/PrintHello.java
@@ -0,0 +1,12 @@
+package com.javaBasics;
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+        System.out.println("Hello, Simran!"); //ln for line break
+		
+		System.out.print("Hello, Simran!"); 
+
+	}
+
+}
