3ebcbdd68cdcaa434d29f978879bf201c9e5516c
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-08-02 18:35:30 +0530

    Switch statement

--- Java/src/javabasics/SwitchSt.java
+++ Java/src/javabasics/SwitchSt.java
@@ -13,28 +13,28 @@ public class SwitchSt {
 		
 		switch(day) {
 			case 1:
-				System.out.print("Monday");
+				System.out.println("Monday");
 				break;
 			case 2: 
-				System.out.print("Tuesday");
+				System.out.println("Tuesday");
 				break;
 			case 3:
-				System.out.print("Wednesday");
+				System.out.println("Wednesday");
 				break;
 			case 4: 
-				System.out.print("Thursday");
+				System.out.println("Thursday");
 				break;
 			case 5:
-				System.out.print("Friday");
+				System.out.println("Friday");
 				break;
 			case 6: 
-				System.out.print("Saturday");
+				System.out.println("Saturday");
 				break;
 			case 7:
-				System.out.print("Sunday");
+				System.out.println("Sunday");
 				break;
 			default: 
-				System.out.print("Not valid value");
+				System.out.println("Not valid value");
 		}
 	}
 }
