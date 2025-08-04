e529c4187a21028f58d7020a0d6c56e2bc89706b
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-08-04 23:35:24 +0530

    Import packages

---
+++ Java/src/oops_concept/PackagesEx.java
@@ -0,0 +1,10 @@
+package oops_concept;
+
+import javabasics.Functions;
+
+public class PackagesEx {
+    public static void main(String[] args) {
+        Functions p1 = new Functions();
+        p1.printfan();
+    }
+}
