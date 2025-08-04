3cab1e3112da2ce25b2f9b9eada8ad02320f1320
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-08-04 14:59:05 +0530

    Single Level Inheritance

---
+++ Java/src/oops_concept/SingleLevelInheritance.java
@@ -0,0 +1,22 @@
+package oops_concept;
+
+class Shapes {
+	public void area() {
+		System.out.println("Displays area"); 
+	}
+}
+
+class Triangles extends Shapes {
+	public void area(int l, int h) {
+		System.out.println("1/2*l*h"); 
+	}
+}
+
+public class SingleLevelInheritance {
+
+	public static void main(String[] args) {
+		// Single lvl of inherit from a base cl
+
+	}
+
+}
