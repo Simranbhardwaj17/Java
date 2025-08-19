42c834d1b34c73f372d975f72a7a8b2bfdb58dca
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-08-19 17:47:47 +0530

    Relational Operators

---
+++ Java/src/t02_operators/RelationalOp.java
@@ -0,0 +1,32 @@
+package t02_operators;
+
+public class RelationalOp {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * returns bpplean val only - T/F
+		 a = 3;
+		 b = 5;
+		               
+		a == b;   -> returns F
+		a != b;   -> returns T 
+		a < b;    -> returns F
+		a > b;   -> returns T 
+		a <= b;   -> returns F
+		a >= b;   -> returns T 
+		
+		*/
+		
+		int hisAge = 20;
+		int herAge = 20;
+		
+		System.out.println(hisAge == herAge);
+		System.out.println(hisAge >= herAge);
+		System.out.println(hisAge <= herAge);
+		System.out.println(hisAge > herAge);
+		
+
+	}
+
+}
