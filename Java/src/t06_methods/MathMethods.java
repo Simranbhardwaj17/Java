ed2630330990f090247c651817e60cbcd7bcb64f
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-09-05 23:56:43 +0530

    Math Methods

---
+++ Java/src/t06_methods/MathMethods.java
@@ -0,0 +1,46 @@
+package t06_methods;
+
+public class MathMethods {
+	
+	public static int minimum() {
+		int a = 4;
+		int b = 6;
+		return a<b ? a : b;
+	}
+	
+	public static int minimum2() {
+		int a = 7;
+		int b = 6;
+		if (a<b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+	
+	//using math fn
+	public static void minimum3() {
+		int a = -68;
+		int b = 68;
+		System.out.println("Minimum number is: " + Math.min(a, b));
+	}
+	
+	public static void mathFunctions() {
+		int x = -8;
+		int y = 8;
+		System.out.println("Square root of y is: " + Math.sqrt(y));
+		System.out.println("Square root of the number is: " + Math.sqrt(4));
+		System.out.println("Absolute val of the number is: " + Math.abs(x));
+		System.out.println("Random val is: " + Math.random() * 6);  //only b/w 0 & 1 (mul by 6 to get no b/w 1 to 6)
+	}
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("Minimum number is: " + minimum());
+		System.out.println("Minimum number is: " + minimum2());
+		minimum3();
+		mathFunctions();
+	}
+
+}
