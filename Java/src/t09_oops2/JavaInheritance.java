62fdafb5ae91d0a01f46fc19abb32738d45e6462
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-09-22 22:38:47 +0530

    Inheritance

---
+++ Java/src/t09_oops2/JavaInheritance.java
@@ -0,0 +1,33 @@
+package t09_oops2;
+
+public class JavaInheritance {
+
+	public static void main(String[] args) {
+		
+		House h1 = new House();
+		h1.parking();
+		System.out.println(h1.plot = 2000);
+		h1.location = "Gujarat";
+		System.out.println("Location is: " + h1.location);
+
+	}
+
+}
+
+class Residence {
+	
+	int cost;
+	String location;
+	int elevators;
+	
+	void parking() {
+		System.out.println("Parking avail");
+	}
+}
+
+class House extends Residence {
+	
+	int plot;
+	String gardenName;
+	
+}
