47b56e2edfa8a38cfd7a14ced01fff2ec9891861
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-09-28 22:31:33 +0530

    Palindrome number

---
+++ Java/src/java_coding_practice/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package java_coding_practice;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+		
+		int org_num = num;
+		int rev = 0;
+		
+		while (num > 0) {
+			rev = rev * 10 + num % 10;
+			num = num / 10;
+		}
+		
+		if (org_num == rev) {
+			System.out.println("Number is palindrome");
+		} else {
+			System.out.println("Number is not a palindrome");
+		}
+	}
+	
+}
