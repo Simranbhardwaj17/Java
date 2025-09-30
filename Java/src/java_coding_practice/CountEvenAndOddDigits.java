cfb16f970f5274221ac5fa23ae74e541cc576d3d
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-09-30 23:24:42 +0530

    Count even and odd digits

---
+++ Java/src/java_coding_practice/CountEvenAndOddDigits.java
@@ -0,0 +1,32 @@
+package java_coding_practice;
+
+import java.util.Scanner;
+
+public class CountEvenAndOddDigits {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		int num = sc.nextInt();
+		
+		int evenCount = 0;
+		int oddCount = 0;
+		
+		while (num > 0) {
+			int rem = num % 10;
+			if (rem % 2 == 0) {
+				evenCount++;
+			} else {
+				oddCount++;
+			}
+			
+			num = num/10;
+		}
+		
+		System.out.println("Number of even digits are: " + evenCount);
+		System.out.println("Number of odd digits are: " + oddCount);
+
+	}
+
+}
