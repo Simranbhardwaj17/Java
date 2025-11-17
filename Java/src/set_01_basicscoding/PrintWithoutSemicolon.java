94e9a19ee11a6e86fb79d05ad25a921d8e197df4
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-11-17 13:06:10 +0530

    Print “Hello World” without semicolon

---
+++ Java/src/set_01_basicscoding/PrintWithoutSemicolon.java
@@ -0,0 +1,18 @@
+package set_01_basicscoding;
+
+public class PrintWithoutSemicolon {
+	
+	public static void main(String[] args) {
+		
+		if (System.out.append("Hello World") == null) {
+            // This condition is never true, but the append method prints the string
+        }
+//		 System.out.println(hi());
+	}
+	
+//	    wrong coz use semicolon
+//		public static String hi() {
+//			return "Hi";
+//		}
+	
+}
