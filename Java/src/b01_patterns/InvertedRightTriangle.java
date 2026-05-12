b8ee0138833b93123ccd4f11bb0310b5d026d26d
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2026-05-12 23:55:14 +0530

    Inverted Right Triangle

---
+++ Java/src/b01_patterns/InvertedRightTriangle.java
@@ -0,0 +1,35 @@
+package b01_patterns;
+
+//2. Inverted Right Triangle
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+
+public class InvertedRightTriangle {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				if (j <= 6-i) {
+					System.out.print(" * ");				
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		// or
+		
+		for (int i = 5; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(" * ");				
+			}
+			System.out.println();
+		}
+	}
+
+}
